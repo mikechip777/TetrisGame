c8fbe9f82cee86773354d9f183a15a9b9fdd350d
mikechip777 <miguelchip115@gmail.com>
2023-05-01 18:44:49 -0400

    MainActivity.java implemented.

--- app/src/main/java/com/example/tetrisgame/MainActivity.java
+++ app/src/main/java/com/example/tetrisgame/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.tetrisgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.tetrisgame.models.GameModelFactory;
+import com.example.tetrisgame.models.GameType;
+import com.example.tetrisgame.presenter.GamePresenter;
+import com.example.tetrisgame.presenter.GameTurn;
+import com.example.tetrisgame.views.GameFrame;
+import com.example.tetrisgame.views.GameViewFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GameFrame gameFrame = findViewById(R.id.game_container);
+        TextView gameScoreText = findViewById(R.id.game_score);
+        TextView gameStatusText = findViewById(R.id.game_status);
+        Button gameCtlBtn = findViewById(R.id.game_ctl_btn);
+
+        GamePresenter gamePresenter = new GamePresenter();
+        gamePresenter.setGameModel(GameModelFactory.newGameModel(GameType.TETRIS));
+        gamePresenter.setGameView(GameViewFactory.newGameView(gameFrame, gameScoreText, gameStatusText, gameCtlBtn));
+
+        Button upBtn = findViewById(R.id.up_btn);
+        Button downBtn = findViewById(R.id.down_btn);
+        Button leftBtn = findViewById(R.id.left_btn);
+        Button rightBtn = findViewById(R.id.right_btn);
+        Button fireBtn = findViewById(R.id.fire_btn);
+
+        upBtn.setOnClickListener(v -> gamePresenter.turn(GameTurn.UP));
+        downBtn.setOnClickListener(v -> gamePresenter.turn(GameTurn.DOWN));
+        leftBtn.setOnClickListener(v -> gamePresenter.turn(GameTurn.LEFT));
+        rightBtn.setOnClickListener(v -> gamePresenter.turn(GameTurn.RIGHT));
+        fireBtn.setOnClickListener(v -> gamePresenter.turn(GameTurn.FIRE));
+
+        gameCtlBtn.setOnClickListener(v -> gamePresenter.changeStatus());
+
+        gamePresenter.init();
     }
 }
---
+++ app/src/main/java/com/example/tetrisgame/views/GameViewFactory.java
@@ -0,0 +1,16 @@
+package com.example.tetrisgame.views;
+
+
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.tetrisgame.presenter.GameView;
+
+public class GameViewFactory {
+    private GameViewFactory(){
+    }
+
+    public static GameView newGameView(GameFrame gameFrame, TextView gameScoreText, TextView gameStatusText, Button gameCtlBtn) {
+        return new GameViewImpl(gameFrame, gameScoreText, gameStatusText, gameCtlBtn);
+    }
+}
